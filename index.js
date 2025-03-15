3552976c4e31b75d6c720cd32ce2cc2f53942966
abdullah ahmed <abdallaroom25@gmail.com>
2025-03-15 03:35:59 +0200

    wait until page load

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Initialization for ES Users
 
+
+document.body.style.opacity = 0
+window.addEventListener("load",()=>{
+
+    
+    document.body.style.opacity = 1
+
+})
 document.querySelector("nav").scrollBy(0,0);
 
   console.log(window.innerWidth)
